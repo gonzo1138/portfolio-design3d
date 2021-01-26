ddaa219026e6575ccc9541364e2868e1182b0dfb
Thomas <gonzo113@bassfreaks.de>
2021-01-26 13:09:37 +0100

    texture-test; problem ahead: even when each side of the cube has an eventlistener, every click on it triggers the link, even for turning the cube..

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 window.onload = function(){
 
+    var allSidesOfTheCube = document.querySelectorAll('shape');
+    console.log(allSidesOfTheCube);
+    allSidesOfTheCube.forEach((item)=>{
+        item.addEventListener("click",function(event){
+            alert(event.target.id);
+        });
+    });
 
 
 }

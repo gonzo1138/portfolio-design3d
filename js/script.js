24c5029074785b1e908e7bb69670c3f9d1c3f114
Thomas <gonzo113@bassfreaks.de>
2021-01-31 21:43:42 +0100

    klick-test

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-window.onload = function(){
+document.addEventListener('DOMContentLoaded', function() {
 
-    var allSidesOfTheCube = document.getElementsByTagName('shape');
-    console.log(allSidesOfTheCube);
-    allSidesOfTheCube.forEach((item)=>{
-        item.addEventListener("click",function(event){
-            alert("Boom!");
-        });
-    });
+    let front = document.getElementById('front');
 
+    console.log("I'm here!");
+    function msg(e){
+        alert("Boom!  " + e);
+        console.log("I'm not here!");
+    }
 
-}
+    front.addEventListener("click", msg);
+
+});

355683729c73ff56a1e38efbfb924cd44e7bf4e2
Thomas <gonzo113@bassfreaks.de>
2021-02-03 22:00:46 +0100

    eventhandler auf shapes funktioniert; koordinaten und klickauswertung werden geliefert;

--- js/script.js
+++ js/script.js
@@ -1,13 +1,73 @@
+/*
+var cube;
+x3dom.runtime.ready = function() {
+    cube = document.getElementById('cube');
+    cube.runtime.enterFrame = function(e) {
+        console.log(e);
+    };
+//    alert("About to render something the first time");
+};
+ */
+function patsch(e){
+    console.log(e.id);
+    console.log(e.clientX);
+    console.log(e.clientY);
+}
+/*
+function down(e){
+    console.log(e);
+}
+
+function up(e){
+    console.log(e);
+}
+*/
+
+var klick=[];
+
+function myname(name){
+    console.log("front clicked");
+    console.log(name.id);
+}
+
+document.onload = function() {
+    // Handle mouseover event on a shape
+    var cube = document.getElementById('cube');
+
+    cube.addEventListener('mousedown', function(event) {
+        console.log("mouse down:");
+        console.log(event.clientX);
+        console.log(event.clientY);
+        klick[0] = event.clientX;
+        klick[1] = event.clientY;
+    }, false);
+
+    cube.addEventListener('mouseup', function(event) {
+        console.log("mouse up:");
+        console.log(event.clientX);
+        console.log(event.clientY);
+        klick[2] = event.clientX;
+        klick[3] = event.clientY;
+        console.log(klick);
+        if(klick[0]==klick[2] && klick[1]==klick[3]){
+            console.log("klicked!");
+        }
+
+        //klickcompare();
+    }, false);
+};
+/*
+function klickcompare(){
+    if(klick[0]===klick[2] && klick[1]===klick[3]){
+        console.log("klicked!");
+    }
+}
+*/
 document.addEventListener('DOMContentLoaded', function() {
 
-    let front = document.getElementById('front');
-
-    console.log("I'm here!");
-    function msg(e){
-        alert("Boom!  " + e);
-        console.log("I'm not here!");
+    let data = document.getElementsByClassName('data');
+    //data.addEventListener("click", msg);
+    if(data[0].id === "nix"){
+        console.log("nix is hier");
     }
-
-    front.addEventListener("click", msg);
-
 });
